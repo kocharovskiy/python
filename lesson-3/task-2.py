cf75b45e420d7c73066f5fd450f969d1da15bf64
kocharovskiy <86925613+kocharovskiy@users.noreply.github.com>
2021-07-15 14:13:39 +0300

    Delete task-2.py

--- lesson-3/task-2.py
+++
@@ -1,8 +0,0 @@
-"""2. Реализовать функцию, принимающую несколько параметров, описывающих данные пользователя:
-имя, фамилия, год рождения, город проживания, email, телефон. Функция должна принимать
-параметры как именованные аргументы. Реализовать вывод данных о пользователе одной строкой."""
-
-def inf(name, surname, year, city, email, phone):
-    return [name, surname, year, city, email, phone]
-
-print(inf(name='Denis', surname='Koch', year='1990', city='Moscow', email='mail@mail.ru', phone='12345'))
